ee54bbd69fce436da8cff624dd2378ff0224cecf
diego <103156696+diegooooop@users.noreply.github.com>
2023-03-11 16:40:48 +0800

    Initial Pages Started

---
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/WelcomePage.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s11.pallasigue.diego.brightsideproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_page)
+    }
+}
