1683d823fe852023edbf86668e9c204f55258e12
Nicole <nicole_dans@dlsu.edu.ph>
2023-03-15 23:36:44 +0800

    colors n views

--- app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/DataGenerator.kt
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/DataGenerator.kt
@@ -10,49 +10,49 @@ class DataGenerator {
                     "Happy",
                     R.drawable.happy,
                     adjectives = arrayOf<String>("Joyful", "Cheerful", "Delighted", "Pleased", "Ecstatic", "Blissful", "Elated", "Grateful", "Content", "Overjoyed"),
-                    color = Color.YELLOW))
+                    color = Color.parseColor("#F8EACD")))
 
                 data.add(MoodModel(
                     "Sad",
-                    R.drawable.sad,
+                    R.drawable.anxious,
                     adjectives = arrayOf<String>("Miserable", "Gloomy", "Melancholy", "Somber", "Depressed", "Sorrowful", "Blue", "Despondent", "Rejected", "Heartbroken"),
-                    color = Color.BLUE))
+                    color = Color.parseColor("#DAEDFC")))
 
                 data.add(MoodModel(
                     "Angry",
                     R.drawable.angry,
                     adjectives = arrayOf<String>("Furious", "Enraged", "Livid", "Irritated", "Incensed", "Fuming", "Seething", "Hostile", "Resentful", "Aggravated"),
-                    color = Color.RED))
+                    color = Color.parseColor("#FFE5CF")))
 
                 data.add(MoodModel(
                     "Bored",
                     R.drawable.bored,
                     adjectives = arrayOf<String>("Uninterested", "Disinterested", "Apathetic", "Uninspired", "Mundane", "Tedious", "Monotonous", "Dull", "Tiresome", "Lethargic"),
-                    color = Color.GREEN))
+                    color = Color.parseColor("#F7CDD0")))
 
                 data.add(MoodModel(
                     "Anxious",
-                    R.drawable.anxious,
+                    R.drawable.nervous,
                     adjectives = arrayOf<String>("Nervous", "Worried", "Apprehensive", "Uneasy", "Restless", "Jittery", "Fidgety", "Panicky", "Tense", "Agitated"),
-                    color = Color.GREEN))
+                    color = Color.parseColor("#F3F7FA")))
 
                 data.add(MoodModel(
                     "Confused",
                     R.drawable.confused,
                     adjectives = arrayOf<String>("Perplexed", "Baffled", "Bewildered", "Puzzled", "Disoriented", "Muddled", "Fuddled", "Dazed", "Flustered", "Mixed-up"),
-                    color = Color.RED))
+                    color = Color.parseColor("#C7E1F8")))
 
                 data.add(MoodModel(
-                    "Inlove",
+                    "In love",
                     R.drawable.inlove,
                     adjectives = arrayOf<String>("Smitten", "Enamored", "Adoring", "Devoted", "Infatuated", "Passionate", "Besotted", "Charmed", "Enchanted", "Amorous"),
-                    color = Color.BLUE))
+                    color = Color.parseColor("#FFF9C7")))
 
                 data.add(MoodModel(
                     "Nervous",
-                    R.drawable.nervous,
+                    R.drawable.anxious,
                     adjectives = arrayOf<String>("Anxious", "Apprehensive", "Uneasy", "Jittery", "Tense", "Fidgety", "Worried", "Agitated", "Restless", "Nervous-wreck"),
-                    color = Color.YELLOW))
+                    color = Color.parseColor("#F8F1FF")))
                 return data
             }
         }
