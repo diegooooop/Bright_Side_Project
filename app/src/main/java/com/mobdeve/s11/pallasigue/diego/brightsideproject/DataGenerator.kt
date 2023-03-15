22c2f3f5cc50f5d58e371caa37b8f0b058e0b64b
kathtan <kathleen_tan@dlsu.edu.ph>
2023-03-15 21:48:47 +0800

    added adjectives in data generator

--- app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/DataGenerator.kt
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/DataGenerator.kt
@@ -11,7 +11,7 @@ class DataGenerator {
                     textColor =
                 ))
                 data.add(MoodModel(
-                    "sad",
+                    "Sad",
                     R.drawable.sad,
                     adjectives = arrayOf<String>("Miserable", "Gloomy", "Melancholy", "Somber", "Depressed", "Sorrowful", "Blue", "Despondent", "Rejected", "Heartbroken")))
                 data.add(MoodModel(
@@ -21,30 +21,23 @@ class DataGenerator {
                 data.add(MoodModel(
                     "Bored",
                     R.drawable.bored,
-                    adjectives = arrayOf<String>("")))
+                    adjectives = arrayOf<String>("Uninterested", "Disinterested", "Apathetic", "Uninspired", "Mundane", "Tedious", "Monotonous", "Dull", "Tiresome", "Lethargic")))
                 data.add(MoodModel(
                     "Anxious",
                     R.drawable.anxious,
-                    adjectives = arrayOf<String>("")))
+                    adjectives = arrayOf<String>("Nervous", "Worried", "Apprehensive", "Uneasy", "Restless", "Jittery", "Fidgety", "Panicky", "Tense", "Agitated")))
                 data.add(MoodModel(
                     "Confused",
                     R.drawable.confused,
-                    adjectives = arrayOf<String>("")))
+                    adjectives = arrayOf<String>("Perplexed", "Baffled", "Bewildered", "Puzzled", "Disoriented", "Muddled", "Fuddled", "Dazed", "Flustered", "Mixed-up")))
                 data.add(MoodModel(
                     "Inlove",
                     R.drawable.inlove,
-                    adjectives = arrayOf<String>("")))
+                    adjectives = arrayOf<String>("Smitten", "Enamored", "Adoring", "Devoted", "Infatuated", "Passionate", "Besotted", "Charmed", "Enchanted", "Amorous")))
                 data.add(MoodModel(
                     "Nervous",
                     R.drawable.nervous,
-                    adjectives = arrayOf<String>("")))
-
-
-
-
-
-
-
+                    adjectives = arrayOf<String>("Anxious", "Apprehensive", "Uneasy", "Jittery", "Tense", "Fidgety", "Worried", "Agitated", "Restless", "Nervous-wreck")))
                 return data
             }
         }
