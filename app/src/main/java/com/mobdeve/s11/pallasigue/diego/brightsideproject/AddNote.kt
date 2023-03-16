a62b260a992e60932f43f854a448abfe87d57be4
Nicole <nicole_dans@dlsu.edu.ph>
2023-03-16 17:29:04 +0800

    notes page

---
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/AddNote.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s11.pallasigue.diego.brightsideproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
