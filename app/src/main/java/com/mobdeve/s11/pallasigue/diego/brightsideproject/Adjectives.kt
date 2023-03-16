f78e235f6ddb4722e86afc794c14eb1515651b48
Nicole <nicole_dans@dlsu.edu.ph>
2023-03-16 20:31:42 +0800

    grid layout adjectives

--- app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/Adjectives.kt
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/Adjectives.kt
@@ -1,15 +1,20 @@
 package com.mobdeve.s11.pallasigue.diego.brightsideproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.internal.ContextUtils.getActivity
+
 
 class Adjectives : AppCompatActivity()
 {
     private lateinit var nameTV: TextView
     private lateinit var recyclerView: RecyclerView
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adjectives)
@@ -22,6 +27,9 @@ class Adjectives : AppCompatActivity()
 
         this.recyclerView = findViewById(R.id.rv_adjective)
         this.recyclerView.adapter = i?.let { AdapterAdjectives(it) }
-        this.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        this.recyclerView.layoutManager = GridLayoutManager(getActivity(applicationContext), 2)
+//            LinearLayoutManager(this)
+//        topicListView.setLayoutManager(GridLayoutManager(getActivity(), 3))
     }
 }
