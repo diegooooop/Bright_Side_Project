69130115fd1ff819718a617d6950bcf4d1851b1c
Nicole <nicole_dans@dlsu.edu.ph>
2023-03-17 00:14:29 +0800

    homepage

--- app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/MoodModel.kt
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/MoodModel.kt
@@ -2,4 +2,4 @@ package com.mobdeve.s11.pallasigue.diego.brightsideproject
 
 import android.graphics.Color
 
-class MoodModel (val name: String, val imageId: Int, val adjectives: Array<String>, val color: Int)
+class MoodModel (val name: String, )
---
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/WeeklyModel.kt
@@ -0,0 +1,3 @@
+package com.mobdeve.s11.pallasigue.diego.brightsideproject
+
+class WeeklyModel (val weekName: String, val imageId: Int)
