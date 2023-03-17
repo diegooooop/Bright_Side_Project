7203e4a8789dccc5f567dad1aa37a5aa7ffe58ca
diego <diego_pallasigue@dlsu.edu.ph>
2023-03-17 17:05:37 +0800

    fixed many thingss

--- app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/SettingsActivity.kt
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/SettingsActivity.kt
@@ -1,11 +1,47 @@
 package com.mobdeve.s11.pallasigue.diego.brightsideproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.EditText
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var switch: Switch
+    private lateinit var time: EditText
+    private lateinit var confbtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        switch = findViewById(R.id.switch1)
+        time = findViewById(R.id.timeEt)
+
+
+        switch.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            // do something, the isChecked will be
+            // true if the switch is in the On position
+            if(isChecked){
+                time.isEnabled = true
+                time.isClickable = true
+                time.isActivated = true
+            }
+            else{
+                time.isEnabled = false
+                time.isClickable = false
+                time.isActivated = false
+            }
+
+        })
+        confbtn = findViewById(R.id.confirmBtn)
+        confbtn.setOnClickListener(View.OnClickListener {
+            val i = Intent(applicationContext, HomePageActivity::class.java)
+            startActivity(i)
+        })
     }
 }
