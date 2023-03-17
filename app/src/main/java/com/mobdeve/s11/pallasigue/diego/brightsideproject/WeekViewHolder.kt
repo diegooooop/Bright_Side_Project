ebdf7f5de95e3ae3e0a84008167dbb28ed82fdef
kathtan <kathleen_tan@dlsu.edu.ph>
2023-03-17 14:14:48 +0800

    changed

--- app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/WeekViewHolder.kt
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/WeekViewHolder.kt
@@ -10,7 +10,6 @@ import org.w3c.dom.Text
 class WeekViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val mood: ImageView = itemView.findViewById(R.id.iv_mood)
     private val day: TextView = itemView.findViewById(R.id.tv_day)
-    private val name: TextView = itemView.findViewById(R.id.tv_moodText)
     private val adjective: TextView = itemView.findViewById(R.id.tv_adjectives)
     private val notes: TextView = itemView.findViewById(R.id.tv_notes)
     private val moodText: TextView = itemView.findViewById(R.id.tv_moodText)
@@ -21,7 +20,6 @@ class WeekViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         mood.setImageResource(data.model.imageId)
         moodText.text = "You are" + data.model.name
         day.text = data.day
-        name.text = data.model.name
         adjective.text= data.model.adjectives.toString()
         notes.text = data.notes
         bgcolor.setBackgroundColor(data.model.color)
