e92c53ad9a64dd66174e1a39a12b6901f422bc00
Nicole <nicole_dans@dlsu.edu.ph>
2023-03-29 15:25:24 +0800

    db helper (not yet integrated)

--- app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/MyDbHelper.kt
+++ app/src/main/java/com/mobdeve/s11/pallasigue/diego/brightsideproject/MyDbHelper.kt
@@ -1,9 +1,16 @@
 package com.mobdeve.s11.pallasigue.diego.brightsideproject
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.io.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class MyDbHelper(context: Context?) : SQLiteOpenHelper(context, DbReference.DATABASE_NAME, null, DbReference.DATABASE_VERSION) {
 
@@ -28,7 +35,8 @@ class MyDbHelper(context: Context?) : SQLiteOpenHelper(context, DbReference.DATA
         onCreate(sqLiteDatabase)
     }
 
-    fun getAllEntriesDefault(): ArrayList<EntryModel> {
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getAllEntriesDefault() {
         val database: SQLiteDatabase = this.readableDatabase
 
         val c: Cursor = database.query(
@@ -43,14 +51,60 @@ class MyDbHelper(context: Context?) : SQLiteOpenHelper(context, DbReference.DATA
         )
 
         val entry: ArrayList<EntryModel> = ArrayList()
-        val mood: MoodModel
+
 
         while(c.moveToNext()){
+            var blob = c.getBlob(c.getColumnIndexOrThrow((DbReference.COLUMN_NAME_MODEL)))
+
+            val current = c.getString(c.getColumnIndexOrThrow((DbReference.COLUMN_NAME_DATE)))
+            val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+            val formatted = LocalDate.parse(current, formatter)
+
         entry.add(EntryModel(
-            c.get
+            formatted,
+            read(blob),
             c.getString(c.getColumnIndexOrThrow(DbReference.COLUMN_NAME_NOTES)),
         ))
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun insertEntry(entry: EntryModel){
+
+        val database = this.writableDatabase
+
+        val values = ContentValues()
+        values.put(DbReference.COLUMN_NAME_NOTES, entry.notes)
+
+//        Formatting date before inserting in database
+        val current = entry.date
+        val formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+        val formatted = current.format(formatter)
+        values.put(DbReference.COLUMN_NAME_DATE, formatted)
+
+        val model = makebyte(entry.model)
+        values.put(DbReference.COLUMN_NAME_MODEL, model)
+
+    }
+
+    fun makebyte(modeldata: MoodModel): ByteArray? {
+        try {
+            val baos = ByteArrayOutputStream()
+            val oos = ObjectOutputStream(baos)
+            oos.writeObject(modeldata)
+            val employeeAsBytes: ByteArray = baos.toByteArray()
+            val bais = ByteArrayInputStream(employeeAsBytes)
+            return employeeAsBytes
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    fun read(data: ByteArray?): MoodModel {
+            val baip = ByteArrayInputStream(data)
+            val ois = ObjectInputStream(baip)
+            return ois.readObject() as MoodModel
     }
 
     private object DbReference{
@@ -61,7 +115,13 @@ class MyDbHelper(context: Context?) : SQLiteOpenHelper(context, DbReference.DATA
         const val _ID = "id"
         const val COLUMN_NAME_DATE = "date"
         const val COLUMN_NAME_NOTES = "notes"
+
         const val COLUMN_NAME_MODEL = "model"
+
+//        const val COLUMN_NAME_MODEL_NAME = "name"
+//        const val COLUMN_NAME_MODEL_IMAGE = "image"
+//        const val COLUMN_NAME_MODEL_ADJECTIVES = "adjectives"
+//        const val COLUMN_NAME_MODEL_COLOR = "color"
 
         const val CREATE_TABLE_STATEMENT =
             "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
